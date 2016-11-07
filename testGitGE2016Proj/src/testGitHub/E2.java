b0fb0935913e13a3ce8ca3e32fc886ff94050d2b
francois <francois@fdb-ThinkPad>
2016-11-07 08:52:15 +0100

    premiere modif E2

---
+++ testGitGE2016Proj/src/testGitHub/E2.java
@@ -0,0 +1,18 @@
+package testGitHub;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author francois
+ */
+public class E2 {
+
+    public void e2() {
+        System.out.println("initial E2");
+    }
+
+}
